4c2e32c3f15b714d877b2b0c7c7c6c200095f9a5
Your Name <you@example.com>
2018-04-21 12:57:21 -0400

    APIbook3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
-  belongs_to :author
+has_many :book_copies
+belongs_to :author
+
+validates :title, :author, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+has_many :book_copies
+
+validates :first_name, :last_name, :email, presence: true
+
 end
