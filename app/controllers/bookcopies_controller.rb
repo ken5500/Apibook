1767fc2a74a1546e14b8b7195b480428e633bebb
Your Name <you@example.com>
2018-04-24 18:20:07 -0400

    book copy routes updated

--- app/controllers/bookcopies_controller.rb
+++ app/controllers/bookcopies_controller.rb
@@ -1,38 +1,32 @@
 module V1
 class BookcopiesController <
-  before_action :set_book_copy, only: [:show, :destroy, :update]
+  class BookCopiesController < ApplicationController
+    skip_before_action :authenticate_admin, only: [:return_book, :borrow]
+    before_action :authenticate, only: [:return_book, :borrow]
+    before_action :current_user_presence, only: [:return_book, :borrow]
+    before_action :set_book_copy, only: [:show, :destroy, :update, :borrow, :return_book]
 
-    def index
-      book_copies = BookCopy.preload(:book, :user, book: [:author]).paginate(page: params[:page])
-      render json: book_copies, meta: pagination(book_copies), adapter: :json
-    end
 
-    def show
-      render json: @book_copy, adapter: :json
-    end
-
-    def create
-      book_copy = BookCopy.new(book_copy_params)
-      if book_copy.save
-        render json: book_copy, adapter: :json, status: 201
-      else
-        render json: { error: book_copy.errors }, status: 422
-      end
-    end
-
-    def update
-      if @book_copy.update(book_copy_params)
+    def borrow
+      if @book_copy.borrow(current_user)
         render json: @book_copy, adapter: :json, status: 200
       else
-        render json: { error: @book_copy.errors }, status: 422
+        render json: { error: 'Cannot borrow this book.' }, status: 422
       end
     end
 
-    def destroy
-      @book_copy.destroy
-      head 204
+    def return_book
+      authorize(@book_copy)
+
+      if @book_copy.return_book(current_user)
+        render json: @book_copy, adapter: :json, status: 200
+      else
+        render json: { error: 'Cannot return this book.' }, status: 422
+      end
     end
 
+
+    
     private
 
     def set_book_copy
@@ -41,53 +35,6 @@ class BookcopiesController <
 
     def book_copy_params
       params.require(:book_copy).permit(:book_id, :format, :isbn, :published, :user_id)
-    end
-  end
-end
-
-module V1
-  class BooksController < ApplicationController
-    before_action :set_book, only: [:show, :destroy, :update]
-
-    def index
-      books = Book.preload(:author, :book_copies).paginate(page: params[:page])
-      render json: books, meta: pagination(books), adapter: :json
-    end
-
-    def show
-      render json: @book, adapter: :json
-    end
-
-    def create
-      book = Book.new(book_params)
-      if book.save
-        render json: book, adapter: :json, status: 201
-      else
-        render json: { error: book.errors }, status: 422
-      end
-    end
-
-    def update
-      if @book.update(book_params)
-        render json: @book, adapter: :json, status: 200
-      else
-        render json: { error: @book.errors }, status: 422
-      end
-    end
-
-    def destroy
-      @book.destroy
-      head 204
-    end
-
-    private
-
-    def set_book
-      @book = Book.find(params[:id])
-    end
-
-    def book_params
-      params.require(:book).permit(:title, :author_id)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
     resources :authors, only: [:index, :create, :update, :destroy, :show]
     resources :books, only: [:index, :create, :update, :destroy, :show]
     resources :book_copies, only: [:index, :create, :update, :destroy, :show]
-    resources :users, only: [:index, :create, :update, :destroy, :show]
+    resources :users, only: [:index, :create, :update, :destroy, :show
+
+    resources :book_copies, only: [:index, :create, :update, :destroy, :show] do
+      member do
+        put :borrow
+        put :return_book
+      end
+    end
   end
 end
